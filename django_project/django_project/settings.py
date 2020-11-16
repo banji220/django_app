27e6f8a857bd97732124bcf155e215b880a58433
Banji <khanjani1997@gmail.com>
2020-11-16 12:01:05 +0330

    Added Comma

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blog.apps.BlogConfig'
+    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
