6270de6cf60381beb519877f7db0eabdac84df87
Banji <khanjani1997@gmail.com>
2020-11-16 13:35:21 +0330

    Added comment on home() function

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
+    # the first Item of render() function should be request and the we define our templates, blog/home.htlm, blog is sub-dir !
+    # request ----> HttpResponse (we should either use HttpResponse or exception!)
     return render(request, "blog/home.html")
 
 def about(request):
