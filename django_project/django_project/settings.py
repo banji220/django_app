7249d223d6a06db54ed7d7229a6c34cd784ad453
Banji <khanjani1997@gmail.com>
2020-11-19 11:25:35 +0330

    Changed the proiority of the users app

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
