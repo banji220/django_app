7dcced28a549d0e90dc03fe17fa25e20108bcd95
Banji <khanjani1997@gmail.com>
2020-11-21 21:33:43 +0330

    after validating our form, it'll redirect to the home page

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
@@ -7,6 +7,8 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data.get("username")
+            messages.success(request, f"Account created for { username }!")
+            return redirect("blog-home")
     else:
         form = UserCreationForm()
     return render(request, "users/register.html", {"form":form})
