5cacd2fa31716b8f0858c2d6f6e9b5e9627e9eb6
Banji <khanjani1997@gmail.com>
2020-11-21 20:22:51 +0330

    Imported messages to use in flash messages

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
 def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
