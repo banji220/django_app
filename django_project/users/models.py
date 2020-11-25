492406d74d18c6bd3a76990b6ade7472f4384cf5
Banji <khanjani1997@gmail.com>
2020-11-25 13:42:10 +0330

    1. Added Class Profile| 2. OneToOne relationship between user and profile

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,2 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    
+    #! OneToOneField ----> means that one user can only have one profile, and viseversa!
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
