77552bc9b936dc4ee7eefac41da63d8a02922aea
Banji <khanjani1997@gmail.com>
2020-11-25 13:49:48 +0330

    Added __str__ method

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -6,3 +6,6 @@ class Profile(models.Model):
     #! OneToOneField ----> means that one user can only have one profile, and viseversa!
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default="default.jpg", upload_to="profile_pics")
+    
+    def __str__(self):
+        return f"{self.user.username} Profile"
