fb169184d0f5f08be4a3c911ce7eceb836f314db
Banji <khanjani1997@gmail.com>
2020-11-25 13:43:26 +0330

    Added ImageField class to our Profile Class

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -5,4 +5,4 @@ class Profile(models.Model):
     
     #! OneToOneField ----> means that one user can only have one profile, and viseversa!
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    
+    image = models.ImageField(default="default.jpg", upload_to="profile_pics")
