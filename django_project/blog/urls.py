dc6e46cb7e4164a913a754f422f5f10cb091f600
Banji <khanjani1997@gmail.com>
2020-11-27 21:05:34 +0330

    Make as_view method executable

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import PostListView
 from  . import views
 urlpatterns = [
-    path('', PostListView.as_view, name="blog-home"),
+    path('', PostListView.as_view(), name="blog-home"),
     path('about/', views.about, name="blog-about")
 ]
