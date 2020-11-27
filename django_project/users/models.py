b3a83cd2602f82635f1f85c8244774c28284890c
Banji <khanjani1997@gmail.com>
2020-11-27 15:50:48 +0330

    Completed Resizing Images to 300*300:

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from PIL import Image
 class Profile(models.Model):
     
     #! OneToOneField ----> means that one user can only have one profile, and viseversa!
@@ -9,3 +9,14 @@ class Profile(models.Model):
     
     def __str__(self):
         return f"{self.user.username} Profile"
+    
+    
+    #* Resizing Images to the 300*300
+    def save(self):
+        super().save()
+        
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
