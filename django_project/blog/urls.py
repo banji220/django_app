87906d5275156686b3918d2257aa140081e31c92
Banji <khanjani1997@gmail.com>
2020-11-29 02:40:46 +0330

    Change the name create to the post-create| Added conditional to the post-detail.html

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post-update"),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="post-delete"),
-    path('post/new/', PostCreateView.as_view(), name="create"),
+    path('post/new/', PostCreateView.as_view(), name="post-create"),
     path('about/', views.about, name="blog-about"),
 ]
