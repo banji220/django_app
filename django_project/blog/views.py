f75d0f1c1c411c5f452571214dafb5479d175e77
Banji <khanjani1997@gmail.com>
2020-11-28 20:48:55 +0330

    Created PostUpdateView

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -32,5 +32,15 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
     
+    
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ["title", "content"]
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+
 def about(request):
     return render(request, "blog/about.html", {"title": "About"})
