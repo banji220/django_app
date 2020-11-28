474cbb3726460633768687f4a4cd521538035597
Banji <khanjani1997@gmail.com>
2020-11-28 16:41:13 +0330

    Users will reverse to the post-detail page after creating their new posts

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -18,3 +18,6 @@ class Post(models.Model):
     def __str__(self):
         return self.title
         
+    
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
