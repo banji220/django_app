b3e876cf032f49bda30158d91a24afb47e3d368c
Banji <khanjani1997@gmail.com>
2020-11-28 21:38:00 +0330

    Defined Url in the urlpatterns

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from  . import views
 urlpatterns = [
     path('', PostListView.as_view(), name="blog-home"),
     #! <pk> -----> mean PrimaryKey or the ID of a Post
     path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post-update"),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="post-delete"),
     path('post/new/', PostCreateView.as_view(), name="create"),
     path('about/', views.about, name="blog-about"),
 ]
