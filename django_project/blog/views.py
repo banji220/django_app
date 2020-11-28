a7e3a0cc09d1c4562638004fbf8ddb27d343eff4
Banji <khanjani1997@gmail.com>
2020-11-28 13:09:49 +0330

    Created PostDetailView and inherit it from DetailView

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -18,6 +18,11 @@ class PostListView(ListView):
     model = Post
     template_name = "blog/home.html"   ### <app> / <model>_<viewtype>.html # blog/post_list.html
     context_object_name = "posts"
+    ordering = ["-date_posted"]
+    
+class PostDetailView(DetailView):
+    model = Post
+    
     
 def about(request):
     return render(request, "blog/about.html", {"title": "About"})
