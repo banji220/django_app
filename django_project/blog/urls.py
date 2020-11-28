1581010a2b8d15b755b0a2249c76de69781ccf48
Banji <khanjani1997@gmail.com>
2020-11-28 21:07:25 +0330

    Defining Update Url in the urls.py

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
 from  . import views
 urlpatterns = [
     path('', PostListView.as_view(), name="blog-home"),
     #! <pk> -----> mean PrimaryKey or the ID of a Post
     path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post-update"),
     path('post/new/', PostCreateView.as_view(), name="create"),
     path('about/', views.about, name="blog-about"),
 ]
