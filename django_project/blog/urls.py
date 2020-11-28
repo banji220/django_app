1b3bbcde2afc336e77b5bd460d1af4d007163f33
Banji <khanjani1997@gmail.com>
2020-11-28 13:22:16 +0330

    Added url pattern for PostDetailView

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView
 from  . import views
 urlpatterns = [
     path('', PostListView.as_view(), name="blog-home"),
-    path('about/', views.about, name="blog-about")
+    #! <pk> -----> mean PrimaryKey or the ID of a Post
+    path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
+    path('about/', views.about, name="blog-about"),
 ]
