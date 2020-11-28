ca914bfb917f09dfa9dabb1d9342c5e0519fab93
Banji <khanjani1997@gmail.com>
2020-11-28 15:33:04 +0330

    1. created a new file for users so they can create new posts ---> post_form.html| 2. Specifying it's url pattern in the project urls.py

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView
 from  . import views
 urlpatterns = [
     path('', PostListView.as_view(), name="blog-home"),
     #! <pk> -----> mean PrimaryKey or the ID of a Post
     path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
+    path('post/new/', PostCreateView.as_view(), name="create"),
     path('about/', views.about, name="blog-about"),
 ]
